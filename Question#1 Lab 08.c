e64c799c079b4143ef3d0bc35d1e6021c0755042
Imran Usmani <k253034@nu.edu.pk>
2025-10-23 21:34:02 -0700

    Add files via upload

---
+++ Question#1 Lab 08.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+	int i, j, n;
+	
+	printf("Enter a Number : ");
+	scanf("%d", &n);
+	for(i=1;i<=n;i++){
+		printf("%d: \t", i);
+		for(j=1;j<=10;j++){
+			printf("%d\t", i*j);
+		}
+		printf("\n");
+	}
+
+	return 0;
+}
